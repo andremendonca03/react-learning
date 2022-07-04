8e92c68302b20446af678b7e68b39099b82a51af
Andre Mendonca <andremendonca.033@gmail.com>
2022-07-04 12:24:42 +1000

    JSX Arrays Prettified

--- reactjs/module2/008-jsx_arrays/App.jsx
+++ reactjs/module2/008-jsx_arrays/App.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const Allproducts = [
   {
     id: 1,
-    name: 'Smartphone',
-    price: 'R$ 2000',
-    colors: ['#29d8d5', '#252a34', '#fc3766'],
+    name: "Smartphone",
+    price: "R$ 2000",
+    colors: ["#29d8d5", "#252a34", "#fc3766"],
   },
   {
     id: 2,
-    name: 'Notebook',
-    price: 'R$ 3000',
-    colors: ['#ffd045', '#d4394b', '#f37c59'],
+    name: "Notebook",
+    price: "R$ 3000",
+    colors: ["#ffd045", "#d4394b", "#f37c59"],
   },
   {
     id: 3,
-    name: 'Tablet',
-    price: 'R$ 1500',
-    colors: ['#365069', '#47c1c8', '#f95786'],
+    name: "Tablet",
+    price: "R$ 1500",
+    colors: ["#365069", "#47c1c8", "#f95786"],
   },
 ];
 
-const products = Allproducts.filter(item => {
+const products = Allproducts.filter((item) => {
   return Number(item.price.slice(3)) > 1500;
 });
 
@@ -35,19 +35,24 @@ const App = () => {
             <p>{item.price}</p>
             <ul>
               {item.colors.map((item) => {
-                return <li key={item} style={{
-                  color: '#FFF',
-                  backgroundColor: item
-                  }}>
-                  {item}
-                </li>
+                return (
+                  <li
+                    key={item}
+                    style={{
+                      color: "#FFF",
+                      backgroundColor: item,
+                    }}
+                  >
+                    {item}
+                  </li>
+                );
               })}
             </ul>
-          </React.Fragment >
-        )
+          </React.Fragment>
+        );
       })}
     </section>
-  )
+  );
 };
 
 export default App;
