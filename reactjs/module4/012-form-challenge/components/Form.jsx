9b27df775f8e8c9bc5edcea267fbace30a232811
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-01 15:29:01 +1000

    Form Update

--- reactjs/module4/012-form-challenge/components/Form.jsx
+++ reactjs/module4/012-form-challenge/components/Form.jsx
@@ -5,19 +5,26 @@ import Field from "./Field";
 const Form = () => {
   const global = React.useContext(GlobalContext);
 
+  const isAllAnswered = global.actualQNumber >= global.questions.length;
+
   function handleSubmit(e) {
     e.preventDefault();
-    if (global.actualQNumber >= 3) {
-      global.setActualQNumber(0);
-    } else {
+    if (!isAllAnswered) {
       global.setActualQNumber((prev) => prev + 1);
+      global.setAnswers((prev) => [...prev, global.checked]);
+      global.setChecked("");
     }
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <Field />
-      <button type="submit">Next</button>
+      {!isAllAnswered && <Field />}
+      {!isAllAnswered && <button type="submit">Next</button>}
+      {isAllAnswered && (
+        <p>
+          Your total score is {global.answers.length}/{global.questions.length}
+        </p>
+      )}
     </form>
   );
 };
