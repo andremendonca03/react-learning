50362fb1799cb8c648c88fae4094bc4009d053e7
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-01 15:28:40 +1000

    Field Update

--- reactjs/module4/012-form-challenge/components/Field.jsx
+++ reactjs/module4/012-form-challenge/components/Field.jsx
@@ -4,9 +4,30 @@ import { GlobalContext } from "../GlobalContext";
 const Field = () => {
   const global = React.useContext(GlobalContext);
 
+  function handleChange({ target }) {
+    global.setChecked(target.value);
+  }
+
   return (
     <fieldset>
       <legend>{global.questions[global.actualQNumber].question}</legend>
+      <pre>
+        {global.questions[global.actualQNumber].options.map((item, index) => {
+          return (
+            <label key={index}>
+              <input
+                type="radio"
+                id={global.actualQNumber}
+                name={`answerQ${global.actualQNumber}`}
+                checked={global.checked === item}
+                value={item}
+                onChange={handleChange}
+              />
+              {item}
+            </label>
+          );
+        })}
+      </pre>
     </fieldset>
   );
 };
