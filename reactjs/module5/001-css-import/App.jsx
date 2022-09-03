ca8bb7900632fa3592df50a45616b9ad923a267e
Andre Mendonca <andremendonca.033@gmail.com>
2022-09-03 15:33:57 +1000

    CSS Import

---
+++ reactjs/module5/001-css-import/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Title from './Title';
+
+const App = () => {
+  return (
+    <div className="container">
+      <p>My website</p>
+      <Title />
+    </div>
+  )
+}
+
+export default App;
