8ab62221bfd46f00b196f12d0d32e9c23bb36f1e
Andre Mendonca <andremendonca.033@gmail.com>
2022-11-05 23:46:00 +1000

    App JSX

--- reactjs/module6/006-router-challenge/App.jsx
+++ reactjs/module6/006-router-challenge/App.jsx
@@ -2,13 +2,33 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
 import Contact from './Contact';
+import Product from './Product';
+import NotFound from './NotFound';
 
 const App = () => {
+  const [products, setProducts] = React.useState(fetchProducts);
+
+  async function fetchProducts() {
+
+    const response = await window.fetch("https://ranekapi.origamid.dev/json/api/produto");
+    const responseJSON = await response.json();
+    setProducts(responseJSON);
+
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/" element={<Home products={products} />} />
+        <Route path="contact" element={<Contact products={products} />} />
+        <Route path="product/*" element={<Product products={products} />}>
+          {products[0] && products.map((item) => {
+            return (
+              <Route path={`${item.id}`} element={<Product products={products} />} key={item.id} />
+            )
+          })}
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
