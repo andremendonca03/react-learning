4225d62b61c9ef4e6f266131bd17afa70f24462d
Andre Mendonca <andremendonca.033@gmail.com>
2022-11-06 15:29:32 +1000

    Contact Update

--- reactjs/module6/006-router-challenge/Contact.jsx
+++ reactjs/module6/006-router-challenge/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import photo from './img/contact.jpg';
+import styles from './Home.module.css';
+import NavBar from './components/NavBar';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className={styles.productsContainer}>
+      <NavBar />
+      <div className={styles.contactImg}>
+        <img src={photo} alt="Contact Us" />
+      </div>
+      <article className={styles.contactInfo}>
+        <h1>Contact Us now</h1>
+        <ul>
+          <li><span>andre@gmail.com</span></li>
+          <li><span>0451 009 999</span></li>
+          <li><span>NextJS st., 4550</span></li>
+        </ul>
+      </article>
+    </section>
   )
 }
 
